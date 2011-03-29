7ae21fb86caad5c9c76b7c5212a4ceca6523ca5d
mmcconnell1618 <marcus@bvsoftware.com>
2011-03-29 08:45:55 -0400

    Initial Web Teaser

---
+++ Teaser/TeaserWeb/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeaserWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string email)
+        {
+            try
+            {
+                if (email.Trim().Length > 5)
+                {
+                    Models.EmailStorage.SaveEmail(email);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Swallow
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Teaser/TeaserWeb/obj/Debug/Package/PackageTmp/Models/EmailStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeaserWeb.Models
+{
+    public class EmailStorage
+    {
+        public static bool SaveEmail(string email)
+        {
+            Data.TeaserDataEntities context = new Data.TeaserDataEntities();
+            Data.TeaserEmail e = new Data.TeaserEmail();
+            e.Email = email;
+            e.TimeStampUtc = DateTime.UtcNow;
+            context.TeaserEmails.AddObject(e);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
