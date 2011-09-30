e0bcbbcb6a72b7bf4285eceaaf7e23b57e3b59c1
Marcus McConnell <marcus@bvsoftware.com>
2011-09-30 16:32:54 -0400

    Finished moving /signup pages to Razor MVC

---
+++ App/MerchantTribeStore/Areas/signup/Models/FeatureSection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MerchantTribeStore.Areas.signup.Models
+{
+    public class FeatureSection
+    {
+        public string Name { get; set; }
+        public List<FeatureDetails> Features { get; set; }
+
+        public FeatureSection()
+        {
+            Name = string.Empty;
+            Features = new List<FeatureDetails>();
+        }
+    }
+}
--- App/MerchantTribeStore/Areas/signup/signupAreaRegistration.cs
+++ App/MerchantTribeStore/Areas/signup/signupAreaRegistration.cs
@@ -14,7 +14,7 @@ namespace MerchantTribeStore.Areas.signup
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
-            // Signup Routes
+            // Signup Content Page Routes
             context.MapRoute("signup-paypal", "paypaloffer", new {controller="MarketingPages", action="paypaloffer"});
             context.MapRoute("signup-tour", "signup/tour", new { controller = "MarketingPages", action = "tour" });
             context.MapRoute("signup-design", "signup/design", new { controller = "MarketingPages", action = "design" });
@@ -23,13 +23,16 @@ namespace MerchantTribeStore.Areas.signup
             context.MapRoute("signup-home", "signup/home", new { controller = "MarketingPages", action = "Home" });
             context.MapRoute("signup-featured", "signup/features", new { controller = "MarketingPages", action = "features" });
             context.MapRoute("signup-themes", "signup/premiumthemes", new { controller = "MarketingPages", action = "themes" });
-            context.MapRoute("signup-promote", "signup/promote", new { controller = "MarketingPages", action = "promote" });
-            context.MapRoute("signup-register", "signup/register/{id}", new { controller = "Register", action = "register" });
-            context.MapRoute("signup-register", "signup/register/JsonCheckStoreName", new { controller = "Register", action = "JsonCheckStoreName" });
-            context.MapRoute("signup-processsignup", "signup/processsignup", new { controller = "Register", action = "processsignup" });
+            context.MapRoute("signup-promote", "signup/promote", new { controller = "MarketingPages", action = "promote" });            
             context.MapRoute("signup-sell", "signup/sell", new { controller = "MarketingPages", action = "sell" });
             context.MapRoute("signup-secure", "signup/secure", new { controller = "MarketingPages", action = "secure" });
 
+            // Registration Routes
+            context.MapRoute("signup-register", "signup/register/{id}", new { controller = "Register", action = "register" });
+            context.MapRoute("signup-register", "signup/register/JsonCheckStoreName", new { controller = "Register", action = "JsonCheckStoreName" });
+            context.MapRoute("signup-processsignup", "signup/processsignup", new { controller = "Register", action = "processsignup" });
+
+            // Wildcard
             context.MapRoute(
                 "signup_default",
                 "signup/{controller}/{action}/{id}",
