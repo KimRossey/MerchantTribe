6cfc1eca8aea7943eb5d701a424a499bd322ac50
Marcus McConnell <marcus@bvsoftware.com>
2011-11-07 13:16:17 -0500

    Built 210 version for release

--- App/src/MerchantTribe.Commerce/WebAppSettings.System.cs
+++ App/src/MerchantTribe.Commerce/WebAppSettings.System.cs
@@ -6,7 +6,7 @@ namespace MerchantTribe.Commerce
 
         const string SUPERADMINEMAIL = "noreply@merchanttribe.com";
         const string APPLICATION_NAME = "MerchantTribe";
-const string APPLICATION_VERSION = "1.0.0.209";
+const string APPLICATION_VERSION = "1.0.0.210";
 
         public static string SuperAdminEmail
         {
