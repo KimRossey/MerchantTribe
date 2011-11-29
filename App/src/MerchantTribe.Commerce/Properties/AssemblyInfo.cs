ec18198b97f4a613b7d0e247b8013f3cfeedd003
Marcus McConnell <marcus@bvsoftware.com>
2011-11-29 17:00:15 -0500

    WebMatrix vs IIS config issues

--- App/src/MerchantTribe.Commerce/Properties/AssemblyInfo.cs
+++ App/src/MerchantTribe.Commerce/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using log4net;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.228")]
-[assembly: AssemblyFileVersion("1.2.0.228")]
+[assembly: AssemblyVersion("1.2.0.229")]
+[assembly: AssemblyFileVersion("1.2.0.229")]
--- App/src/MerchantTribe.Commerce/WebAppSettings.System.cs
+++ App/src/MerchantTribe.Commerce/WebAppSettings.System.cs
@@ -6,7 +6,7 @@ namespace MerchantTribe.Commerce
 
         const string SUPERADMINEMAIL = "noreply@merchanttribe.com";
         const string APPLICATION_NAME = "MerchantTribe";
-const string APPLICATION_VERSION = "1.2.0.228";
+const string APPLICATION_VERSION = "1.2.0.229";
 
         public static string SuperAdminEmail
         {
