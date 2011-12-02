1521f36866ca8b2a6b0ebf0abbd025e93d9d9223
Marcus McConnell <marcus@bvsoftware.com>
2011-12-02 13:55:08 -0500

    Build 233 for Web PI

--- App/src/MerchantTribe.Payment/Properties/AssemblyInfo.cs
+++ App/src/MerchantTribe.Payment/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.232")]
-[assembly: AssemblyFileVersion("1.2.0.232")]
+[assembly: AssemblyVersion("1.2.0.233")]
+[assembly: AssemblyFileVersion("1.2.0.233")]
 [assembly: CLSCompliant(true)]
 
 
